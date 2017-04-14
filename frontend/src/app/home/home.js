a0e8cf8c0f89d3387c5ac302c08c9a9e0f25d207
zJoyceLee <zJoyceLee@yeah.net>
2017-04-14 15:10:53 +0800

    mock home datas.

--- frontend/src/app/home/home.js
+++ frontend/src/app/home/home.js
@@ -6,7 +6,19 @@ angular
     controllerAs: 'page'
   });
 
-function Controller() {
+function Controller($log) {
   const vm = this;
   vm.msg = 'Hello, Home.';
+  vm.homelst = [
+    {title: '[晓说]高晓松深挖2017奥斯卡 感慨颁奖小年佳作少', href: 'http://v.youku.com/v_show/id_XMjcwMTU3OTQyOA==.html'},
+    {title: '人民的名义 TV版', href: 'http://v.youku.com/v_show/id_XMjcwNjcwODY1Mg==.html'},
+    {title: '鲜肉老师', href: 'http://v.youku.com/v_show/id_XMjY2NjM5MjE3Ng==.html'},
+    {title: '三生三世十里桃花', href: 'http://v.youku.com/v_show/id_XMjQ4MTc0ODMyOA==.html'}
+  ];
+  vm.homesearch = function (i) {
+    $log.log('search', i);
+  };
+  vm.homedownload = function (i) {
+    $log.log('download', i);
+  };
 }
