e2c0ed3c69079f98671cc2dc95d852dfe25032a7
zJoyceLee <zJoyceLee@yeah.net>
2017-05-11 02:09:10 +0800

    Dnamically get serie.

--- frontend/src/app/online/online.js
+++ frontend/src/app/online/online.js
@@ -16,57 +16,39 @@ angular.module('app.online', [
 Controller.$inject = ['$sce', '$timeout', '$http', '$log', 'myService'];
 function Controller($sce, $timeout, $http, $log, myService) {
   const vm = this;
+  vm.getting = false;
   let global = {};
   if (_.isEmpty(myService.get())) {
-    global = {'url': 'http://v.youku.com/v_show/id_XMjY3MTQ2MDE0OA==.html', 'playlist': []};
+    global = {'url': 'http://v.youku.com/v_show/id_XMjY3MTQ2MDE0OA==.html', 'serie': []};
   } else {
     global = myService.get();
   }
+  $log.log(global);
   vm.myurl = global.url;
 
-  $log.log('global', global);
-
   vm.getSerie = function () {
+    vm.getting = true;
     $log.log('get serie click...');
-
+    $http({
+      method: 'POST',
+      url: 'http://127.0.0.1:5000/serie',
+      data: {
+        url: global.url
+      }
+    }).then((response) => {
+      $log.log('post success...get serie...');
+      $log.log(response.data);
+      vm.serie = response.data.serie;
+      global.serie = vm.serie;
+      myService.set(global);
+      $log.log(global);
+      vm.complete = true;
+    }, () => {
+      $log.log('post failure...do not get serie...');
+      vm.complete = true;
+    });
   };
-  vm.playlst = [
-    {href: 'http://v.youku.com/v_show/id_XMjY3MTQ2MDE0OA==.html', text: '1', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY3NDYyMTUwMA==.html', text: '2', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY3NzU2MTA2MA==.html', text: '3', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY3Nzc1NTY5Ng==.html', text: '4', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY3OTk4NzM2NA==.html', text: '5', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4MjA5MDIxMg==.html', text: '6', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4Mzg5ODI5Mg==.html', text: '7', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4NDAyMDQ1Mg==.html', text: '8', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4NTYyMjIyMA==.html', text: '9', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4NTc0NDQwOA==.html', text: '10', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4NzQxODYwMA==.html', text: '11', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4NzU0NDI1Mg==.html', text: '12', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4OTQ5ODI4MA==.html', text: '13', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY4OTcyOTUwOA==.html', text: '14', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5MTc1MjQ0MA==.html', text: '15', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5MTkwOTU2OA==.html', text: '16', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5MzkwOTQwOA==.html', text: '17', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5NTgzMTE0NA==.html', text: '18', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5NzgxOTgwMA==.html', text: '19', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjY5Nzk0NTk5Mg==.html', text: '20', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwMDAzNzk5Mg==.html', text: '21', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwMDE4MjMwOA==.html', text: '22', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwMjI3ODc1Ng==.html', text: '23', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwMjQxMzYwNA==.html', text: '24', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNDQyNDUwOA==.html', text: '25', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNDU0NjAwMA==.html', text: '26', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjQ1MjQ4OA==.html', text: '27', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjcwODY1Mg==.html', text: '28', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDg3Mg==.html', text: '29', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDg5Mg==.html', text: '30', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDk3Mg==.html', text: '31', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjU3NzQwOA==.html', text: '32', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDgwOA==.html', text: '33', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDc4OA==.html', text: '34', title: null},
-    {href: 'http://v.youku.com/v_show/id_XMjcwNjg2MDc4MA==.html', text: '35', title: null}
-  ];
+
   function inLst(url, lst) {
     let ret = false;
     $.map(lst, (o) => {
@@ -108,8 +90,15 @@ function Controller($sce, $timeout, $http, $log, myService) {
   vm.search = function () {
     global.url = vm.myurl;
     myService.set(global);
-    $log.log(global);
-    $log.log('searched url', vm.myurl);
+
+    $log.log(inLst(global.url, global.serie));
+    if (inLst(global.url, global.serie)) {
+      vm.serie = global.serie;
+    } else {
+      vm.serie = [];
+      vm.getSerie();
+    }
+
     // reset
     vm.loadcomplete = false;
     vm.success = false;
@@ -158,7 +147,6 @@ function Controller($sce, $timeout, $http, $log, myService) {
           }
         };
         vm.inlst = inLst(vm.myurl, vm.playlst);
-      } else {
       } // endif vm.haveMP4
 
 
@@ -169,6 +157,7 @@ function Controller($sce, $timeout, $http, $log, myService) {
       vm.loadcomplete = true;
     });
   };
+
   ($.isEmptyObject(myService.get())) || vm.search();
 
   vm.select = function (i) {
