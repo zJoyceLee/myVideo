bbe7740cfe31232050eca71999aa1e97d340ae6e
zJoyceLee <zJoyceLee@yeah.net>
2017-05-11 21:34:25 +0800

    Remove unuseful route.

--- backend/webserver.py
+++ backend/webserver.py
@@ -8,22 +8,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def hello():
-    # subprocess.call(['which', 'python3'])
-    out = subprocess.check_output(['ls', '-l'])
-    print(type(out), out)
-    return "hello, Flask"
-'''
-@app.route("/<name>")
-def hello_name(name):
-    return "hello, {}".format(name)
-'''
-@app.route('/test', methods=['GET'])
-def test():
-    print('get')
-    # return jsonify({'url_lst': url_lst})
-
 
 def parseRelUrl(mystr):
     ret = {}
