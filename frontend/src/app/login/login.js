b9244b7406512de8e44a675d04a748e1a3f61217
zJoyceLee <zJoyceLee@yeah.net>
2017-05-17 01:40:36 +0800

    login frontend. @todo: .

--- frontend/src/app/login/login.js
+++ frontend/src/app/login/login.js
@@ -7,8 +7,8 @@ angular.module('app')
     controllerAs: 'page'
   });
 
-Controller.$inject = ['$log'];
-function Controller($log) {
+Controller.$inject = ['$log', '$http'];
+function Controller($log, $http) {
   const vm = this;
   vm.isRegister = false;
   vm.switch = function () {
@@ -16,15 +16,43 @@ function Controller($log) {
   };
   // vm.username;
   // vm.passwd;
-  vm.remember = false;
+  vm.gender = false;
+  // vm.remember = false;
   vm.login = function () {
     // vm.isRegister = false;
-    $log.log(vm.username, vm.passwd, vm.remember);
+    $log.log(vm.username, vm.passwd);
+    $http({
+      method: 'POST',
+      url: 'http://127.0.0.1:5000/login',
+      data: {
+        username: vm.username,
+        passwd: vm.passwd
+      }
+    }).then(() => {
+
+    }, () => {
+
+    });
   };
   vm.register = function () {
+    $log.log(vm.gender, vm.age);
     // vm.isRegister = true;
-    if (!_.isEmpty(vm.username) && !_.isEmpty(vm.passwd) && _.isEqual(vm.passwd, vm.confirmpasswd)){
-      $log.log(vm.username, vm.passwd, vm.confirmpasswd, vm.remember);
+    if (!_.isEmpty(vm.username) && !_.isEmpty(vm.passwd)  && (vm.gender!==false)  && !_.isEmpty(vm.age) && _.isEqual(vm.passwd, vm.confirmpasswd)) {
+      $log.log(vm.username, vm.passwd, vm.confirmpasswd, vm.gender, vm.age);
+      // $http({
+      //   method: 'POST',
+      //   url: 'http://127.0.0.1:5000/login',
+      //   data: {
+      //     username: vm.username,
+      //     passwd: vm.passwd,
+      //     gender: vm.gender,
+      //     age: vm.age
+      //   }
+      // }).then(() => {
+
+      // }, () => {
+
+      // });
     }
     $log.log('click register.');
   };
